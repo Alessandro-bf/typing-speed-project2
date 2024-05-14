2290796f232a68b7c90b4cbe56479e3010ba0be2
Alessandro Berganza <alessandrobf@gmail.com>
2024-05-14 10:26:52 +0000

    Add function getRandomText to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-function getRandomText () {
+let texts = [
+    "A journey of a thousand miles begins with a single step.",
+    "Curiosity killed the cat, but satisfaction brought it back.",
+    "Every man is the architect of his own fortune.",
+    "You can lead a horse to water, but you can not make it drink.",
+    "A bird in the hand is worth two in the bush.",
+    "When the going gets tough, the tough get going.",
+    "Do not count your chickens before they are hatched."
+]
 
+function getRandomText () {
+    let randomIndex = Math.floor(Math.random()*texts.length);
+    return texts[randomIndex];    
 }
 
 function startTest () {
